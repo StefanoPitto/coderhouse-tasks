8364083d1479677b5be3cf67848b2d2a0f2d8afc
StefanoPitto <stefapitto@hotmail.com.ar>
2023-01-11 13:39:07 -0300

    delete product added and update product fixed

--- ProductManager.js
+++ ProductManager.js
@@ -51,33 +51,39 @@ class ProductManager {
     return toReturn;
   };
 
+  deleteProduct = (id) => {
+    this.products = this.products.filter((elem) => elem.id !== id);
+    this.updateFile();
+  };
+
   updateProduct = (id, product) => {
     let index = this.products.findIndex((elem) => elem.id === id);
     if (index === -1) return;
-    this.products[index] = { ...product, id };
+    this.products[index] = { ...this.products[index], ...product, id };
     this.updateFile();
   };
 }
 
 const manager = new ProductManager(path);
 
-// manager.addProduct(
-//   "PRIMER PRODUCTO",
-//   "PRIMER PRODUCTO",
-//   "PRIMER PRODUCTO",
-//   "PRIMER PRODUCTO",
-//   "11111111111",
-//   20
-// );
-// manager.addProduct(
-//   "segundo PRODUCTO",
-//   "segundo PRODUCTO",
-//   "segundo PRODUCTO",
-//   "segundo PRODUCTO",
-//   "2222222222",
-//   20
-// );
-// console.log("Products1", manager.getProducts());
-// manager.updateProduct(1, { code: "777777777" });
-// console.log("Products2",manager.getProducts());
-// console.log(manager.getProductById(0));
+manager.addProduct(
+  "PRIMER PRODUCTO",
+  "PRIMER PRODUCTO",
+  "PRIMER PRODUCTO",
+  "PRIMER PRODUCTO",
+  "11111111111",
+  20
+);
+manager.addProduct(
+  "segundo PRODUCTO",
+  "segundo PRODUCTO",
+  "segundo PRODUCTO",
+  "segundo PRODUCTO",
+  "2222222222",
+  20
+);
+manager.deleteProduct(0);
+console.log("Products1", manager.getProducts());
+manager.updateProduct(1, { code: "777777777" });
+console.log("Products2", manager.getProducts());
+console.log(manager.getProductById(0));
